713bfd2934151851fb3d785326504d8848c9cdb1
Chen Jialing <545187560@qq.com>
2026-03-10 22:42:44 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export', // 强制生成纯静态文件，跳过 API 路由
+  trailingSlash: true, // 避免静态导出的 404 问题
 };
-
 export default nextConfig;
